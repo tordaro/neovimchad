982e0308589e4840cf05c98319d1ad28b25e1e07
Tord Aronsen <tordaro@gmail.com>
2023-09-30 16:50:50 +0200

    use mypy

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -17,22 +17,23 @@ local sources = {
 
   -- Python
   b.formatting.black,
-
+  b.diagnostics.mypy,
+  -- b.diagnostics.ruff,
 }
 
 null_ls.setup {
   debug = true,
   sources = sources,
   on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                  vim.lsp.buf.format({ async = false })
-                end,
-            })
-        end
-    end,
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { async = false }
+        end,
+      })
+    end
+  end,
 }
